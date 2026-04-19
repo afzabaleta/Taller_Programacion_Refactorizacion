247f35b205fcf1ba7aae3a6c4aa1b29f1ee77e4c
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-04-19 00:03:19 -0500

    refactor: Se creo la clase Factura

---
+++ RestauranteElBuenSabo/src/main/java/com/mycompany/restauranteelbuensabor/Factura.java
@@ -0,0 +1,49 @@
+package com.mycompany.restauranteelbuensabor;
+
+public class Factura {
+    private final Pedido pedido;
+    private final int numero;
+
+    public Factura(Pedido pedido, int numero) {
+        this.pedido = pedido;
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public double calcularSubtotal() {
+        return pedido.calcularSubtotal();
+    }
+
+    public double calcularDescuento() {
+        if (pedido.contarItemsDiferentes() > Configuracion.MIN_ITEMS_DESCUENTO) {
+            return calcularSubtotal() * Configuracion.TASA_DESCUENTO;
+        }
+        return 0;
+    }
+
+    public double calcularSubtotalConDescuento() {
+        return calcularSubtotal() - calcularDescuento();
+    }
+
+    public double calcularIVA() {
+        return calcularSubtotalConDescuento() * Configuracion.TASA_IVA;
+    }
+
+    public double calcularPropina() {
+        if (calcularSubtotalConDescuento() > Configuracion.UMBRAL_PROPINA) {
+            return (calcularSubtotalConDescuento() + calcularIVA()) * Configuracion.TASA_PROPINA;
+        }
+        return 0;
+    }
+
+    public double calcularTotal() {
+        return calcularSubtotalConDescuento() + calcularIVA() + calcularPropina();
+    }
+}
